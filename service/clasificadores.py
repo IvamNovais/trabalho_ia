7acebe6f70c861382febce0b62f03f010c55bf3b
Ivam Novais <ivamnovaisivam@gmail.com>
2021-07-19 09:48:40 -0300

    inicio do projeto

---
+++ service/clasificadores.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+from sklearn import linear_model
+def getClasificador():
+    data = pd.read_csv("./aluguel.csv")
+    mapping = LabelEncoder()
+    data["cidade_Map"] = mapping.fit_transform(data["cidade"])
+    remover = ['cidade',"aluguel",'total']
+    x = data.drop(remover,axis=1)
+    x = x.values
+    y = data['total']
+    y = y.values
+    clf = linear_model.LinearRegression()
+    return clf.fit(x,y)
+
+def getPreticao(dados):
+    return getClasificador().predict(dados)[0]
